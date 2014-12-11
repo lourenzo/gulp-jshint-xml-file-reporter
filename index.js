c018675178b7afb31e6df97c8d35d2101abdbc2d
Lourenzo Ferreira <lourenzo@gmail.com>
2014-12-11 16:57:58 -0600

    Refactor to support late file writes
    It’s needed to work with gulp-jshint

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 'use strict';
 
-var fs = require('fs'),
-    xmlEmitter = require('./lib/jslint_xml_emitter.js'),
-    defaultFilePath = 'jshint.xml',
-    outStream, filePath;
+var fs = require('fs');
+
+function reset() {
+    exports.out = [];
+    exports.xmlEmitter = null;
+    exports.opts = {};
+} reset();
+
+/**
+ * Load a formatter
+ * @param  {String} formatterPath
+ * @return
+ */
+function loadFormatter(formatterPath) {
+    return require('./lib/' + formatterPath + '_emitter');
+}
 
 /**
  * Write out a XML file for the encountered results
  */
-module.exports = function (results, data, opts) {
+exports.reporter = function (results, data, opts) {
+    console.log(arguments);
+    console.log('\n\n\n\n');
     opts = opts || {};
-    opts.filePath = opts.filePath || defaultFilePath;
+    opts.format = opts.format || 'checkstyle';
+    opts.filePath = opts.filePath || 'jshint.xml';
+    exports.opts = opts;
+    exports.xmlEmitter = loadFormatter(opts.format);
+    exports.out.push(exports.xmlEmitter.formatContent(results));
+};
 
-    if (outStream && filePath !== opts.filePath) {
-        outStream.end();
-        outStream = null;
-    }
-
-    if (!outStream) {
-        outStream = fs.createWriteStream(opts.filePath);
-        filePath = opts.filePath;
-    }
-
-    var xmlData = xmlEmitter(results);
-
-    outStream.write(xmlData);
+exports.writeFile = function () {
+    var outStream = fs.createWriteStream(exports.opts.filePath);
+    outStream.write(exports.xmlEmitter.getHeader());
+    outStream.write(exports.out.join('\n'));
+    outStream.write(exports.xmlEmitter.getFooter());
+    reset();
 };
