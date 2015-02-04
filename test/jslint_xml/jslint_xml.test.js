7d197d744ed3d6158d4c6a050feb8e273b80b324
Aaron Faber <Aaron.Faber@digitaslbi.com>
2015-02-04 09:24:44 +0000

    Moved the tests suites for the different emitters into their own test folders

--- test/jslint_xml/jslint_xml.test.js
+++ test/jslint_xml/jslint_xml.test.js
@@ -2,14 +2,14 @@
 
 var should = require('should'),
     fs = require('fs'),
-    xmlEmitter = require('../lib/jslint_xml_emitter.js');
+    xmlEmitter = require('../../lib/jslint_xml_emitter.js');
 
 describe('jslint_xml', function () {
     it('should transform JSHint results in jslint-compatible XML', function (done) {
-        var errors = require('./fixtures/errors'),
+        var errors = require('../fixtures/errors'),
             xmlText = xmlEmitter(errors);
 
-        fs.readFile('./test/fixtures/errors.xml', function (err, data) {
+        fs.readFile('../fixtures/errors.xml', function (err, data) {
             if (err) return done(err);
             xmlText.should.equal(data.toString());
             done();
