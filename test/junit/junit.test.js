7354f3131c810859a5642991089f29e84a1a86ad
Aaron Faber <Aaron.Faber@digitaslbi.com>
2015-02-09 11:18:59 +0000

    Updated test suite to make sure we're covering the case where there are no lint errors

--- test/junit/junit.test.js
+++ test/junit/junit.test.js
@@ -8,23 +8,50 @@ describe('junit', function () {
     var mockXMLResults;
     var xmlText;
 
-    before(function (done) {
-        fs.readFile('./test/junit/fixtures/mock.xml', function (err, data) {
-            if (err) return done(err);
+    function getXMLMock(path, done) {
+        fs.readFile(path, function (err, data) {
+            if (err) return done (err);
             mockXMLResults = data.toString('utf8');
             done();
         });
+    }
+
+    describe('Given there are no failing tests', function () {
+        before(function (done) {
+            var path = './test/junit/fixtures/mock_empty.xml';
+            getXMLMock(path, done);
+        });
+
+        beforeEach(function () {
+            var errors = require('./fixtures/no_errors');
+
+            xmlText = xmlEmitter.getHeader(errors);
+            xmlText = xmlText.concat(xmlEmitter.formatContent(errors));
+            xmlText = xmlText.concat(xmlEmitter.getFooter());
+        });
+
+        it('should generate a skeleton JSHint results in junit XML', function () {
+            xmlText.should.equal(mockXMLResults);
+        });
     });
 
-    beforeEach(function () {
-        var errors = require('./fixtures/errors');
+    describe('Given there are failing tests', function () {
+        before(function (done) {
+            var path = './test/junit/fixtures/mock.xml';
+            getXMLMock(path, done);
+        });
 
-        xmlText = xmlEmitter.getHeader(errors);
-        xmlText = xmlText.concat(xmlEmitter.formatContent(errors));
-        xmlText = xmlText.concat(xmlEmitter.getFooter());
+        beforeEach(function () {
+            var errors = require('./fixtures/errors');
+
+            xmlText = xmlEmitter.getHeader(errors);
+            xmlText = xmlText.concat(xmlEmitter.formatContent(errors));
+            xmlText = xmlText.concat(xmlEmitter.getFooter());
+        });
+
+        it('should transform JSHint results in junit XML', function () {
+            xmlText.should.equal(mockXMLResults);
+        });
     });
 
-    it('should transform JSHint results in junit XML', function () {
-        xmlText.should.equal(mockXMLResults);
-    });
 });
